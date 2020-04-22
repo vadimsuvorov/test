46b92241ade4c71521ae7b359a2c4017f321a39d
vadimsuvorov <64136591+vadimsuvorov@users.noreply.github.com>
2020-04-22 21:13:42 +0700

    123456

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
 import telebot;
 bot = telebot.TeleBot('884286980:AAEWz7tuxgfCfQm6EYCs_mUt--5PkRlwwsg');
-
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-
-    if message.text == "Привет":
-
-        bot.send_message(message.from_user.id, "Привет, сейчас я расскажу тебе гороскоп на сегодня.")
-
-    elif message.text == "/help":
-
-        bot.send_message(message.from_user.id, "Напиши Привет")
-
-    else:
-
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+print('Hi!')
